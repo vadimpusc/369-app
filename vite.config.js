50c2b60b0d3167661df7494575c90932f2976d8e
Hank Orion <hankorion@Hanks-MacBook-Air.local>
2025-12-04 09:09:41 +0000

    12

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/369-app/",
+export default defineConfig(() => ({
+  base: "369-app",  
   plugins: [svelte()],
 }));
